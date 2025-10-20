d0a9ccfce5a4d2432c1ace334891ad62c2342b3e
AmandyUchiha <mandinha22amanda@gmail.com>
2025-10-20 14:19:32 -0300

    testevocional-backup

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -5,5 +5,5 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-// Este código CRIA a conexão
+// Este código CRIA a 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
