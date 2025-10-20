57a2b72f62a5dacd9889f59ed5abac013cc2b61f
AmandyUchiha <mandinha22amanda@gmail.com>
2025-10-20 14:20:03 -0300

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 // src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js'
 
-// Este código LÊ as variáveis do arquivo .env
+// Este código LÊ as variáveis do arquivo
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
